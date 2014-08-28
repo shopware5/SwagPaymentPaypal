03a95652e16c47af1e640ffb8ce2f2c44976c9a0
Heiner Lohaus <shopware6>
2014-08-28 16:48:06 +0200

    PT-2091 Fix first install

--- Bootstrap.php
+++ Bootstrap.php
@@ -168,6 +168,9 @@ EOD;
 
     protected function fixPluginDescription()
     {
+        if($this->Payment() === null) {
+            return;
+        }
         $newLogo = '<!-- PayPal Logo -->' .
             '<a onclick="window.open(this.href, \'olcwhatispaypal\',\'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=400, height=500\'); return false;"' .
             '    href="https://www.paypal.com/de/cgi-bin/webscr?cmd=xpt/cps/popup/OLCWhatIsPayPal-outside" target="_blank">' .

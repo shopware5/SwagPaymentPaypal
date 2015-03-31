a139da2db3f1ea5dac6e159d04b5d5561ec7fa8b
Heiner Lohaus <hl@shopware.com>
2015-03-31 12:37:34 +0200

    PT-2909 Fix payment in logo in order emails

--- Bootstrap.php
+++ Bootstrap.php
@@ -281,7 +281,7 @@ EOD;
                 '<a onclick="window.open(this.href, \'olcwhatispaypal\',\'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=400, height=500\'); return false;"' .
                 '    href="https://www.paypal.com/de/cgi-bin/webscr?cmd=xpt/cps/popup/OLCWhatIsPayPal-outside" target="_blank" ' .
                 '    class="paypal-logo">' .
-                '<img src="{link file="frontend/_resources/images/paypal_logo.png"}" alt="Logo \'PayPal empfohlen\'">' .
+                '<img src="{link file="media/image/paypal_logo.png"}" alt="Logo \'PayPal empfohlen\'">' .
                 '</a>' . '<!-- PayPal Logo -->' .
                 'Bezahlung per PayPal - einfach, schnell und sicher.'
         ));

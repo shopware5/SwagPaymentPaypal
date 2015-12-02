3ea97fed7ade0b38f74f7bfdc836a8f23abdbec5
Alexander Voss <ap@shopware.com>
2015-12-02 10:50:53 +0100

    PT-4284 - remove duplicate logo

--- Bootstrap.php
+++ Bootstrap.php
@@ -302,7 +302,7 @@ EOD;
         '<a onclick="window.open(this.href, \'olcwhatispaypal\',\'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=400, height=500\'); return false;"' .
         ' href="https://www.paypal.com/de/cgi-bin/webscr?cmd=xpt/cps/popup/OLCWhatIsPayPal-outside" target="_blank">' .
         '<img src="{link file=\'frontend/_resources/images/paypal_logo.png\' fullPath}" alt="Logo \'PayPal empfohlen\'">' .
-        '</a>' . '<!-- PayPal Logo -->';
+        '</a><br>' . '<!-- PayPal Logo -->';
     }
 
     /**

da243fc0e890c0f0053056728380f52b81e85e3f
Patrick Stahl <P.Stahl@shopware.com>
2016-07-27 12:06:48 +0200

    PT-6366 - Fix CSRF token issue when paypal calls the notify-action

---
+++ Components/CSRFWhitelistAware.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * (c) shopware AG <info@shopware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Shopware\Components;
+
+if (!interface_exists('\Shopware\Components\CSRFWhitelistAware')) {
+    interface CSRFWhitelistAware {}
+}
